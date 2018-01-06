1312f0117f257ab8b4e81f668fd53a359894b2f1
aleksander <aleksanderczeso@gmail.com>
2018-01-06 16:24:53 +0100

    Imlementation of separating signs from a simple CAPTCHA image.

---
+++ CAPTCHA_to_signs.py
@@ -0,0 +1,105 @@
+import numpy as np
+from save_images import NewSign
+
+
+class DFS:
+
+    """class finding bitmaps of separated
+     signs in passed image"""
+
+    def __init__(self, img):
+        self.rows = img.shape[0]
+        self.cols = img.shape[1]
+        self.array = np.zeros((self.rows, self.cols))
+        self.bin_array(img)
+        self.visited = np.zeros((self.rows+1, self.cols+1))
+        self.pixels = None
+
+    def black_or_white(self, A):
+
+        """convert a single array
+        to black-or-white byte"""
+
+        return np.sign(765 - np.sum(A))
+
+    def bin_array(self, img):
+
+        """convert rgb array to
+        binary black-or-white array
+        black: 1, white: 0"""
+
+        for x in range(self.rows):
+            for y in range(self.cols):
+                self.array[x, y] = \
+                    np.array(self.black_or_white(img[x, y]))
+
+    def neighbours(self, point):
+
+        """define Moore's neighbours
+        in (point_x, point_y"""
+
+        point_x, point_y = point[0], point[1]
+
+        if point_x == 0 and point_y == 0:
+            return (0, 1), (1, 1), (1, 0)
+        if point_x == self.rows-1 and point_y == \
+            self.cols-1:
+            return (point_x-1, point_y), \
+                   (point_x-1, point_y-1), \
+                   (point_x, point_y-1)
+        if point_x == self.rows-1 and point_y == 0:
+            return (point_x-1, 0), (point_x-1, 1), \
+                   (point_x, 1)
+        if point_x == 0 and point_y == self.cols-1:
+            return (0, point_y-1), (1, point_y-1), \
+                   (1, point_y)
+        if point_x == 0:
+            return (0, point_y - 1), (1, point_y-1), \
+                   (1, point_y), (1, point_y+1), (0, point_y+1)
+        if point_y == 0:
+            return (point_x-1, 0), (point_x-1, 1), \
+                   (point_x, 1), (point_x+1, 1), (point_x+1, 0)
+        if point_x == self.rows-1:
+            return (point_x-1, point_y), (point_x-1, point_y-1), \
+                   (point_x, point_y-1), (point_x-1, point_y+1), \
+                   (point_x, point_y+1)
+        if point_y == self.cols-1:
+            return (point_x, point_y-1), (point_x-1, point_y-1), \
+                   (point_x-1, point_y), (point_x+1, point_y-1), \
+                   (point_x+1, point_y)
+
+        return (point_x-1, point_y-1), (point_x-1, point_y), \
+               (point_x-1, point_y+1), (point_x, point_y+1), \
+               (point_x+1, point_y+1), (point_x+1, point_y), \
+               (point_x+1, point_y-1), (point_x, point_y-1)
+
+    def dfs(self, point):
+
+        """dfs on plane with Moore's
+        neighbourhood"""
+
+        self.visited[point[0], point[1]] = 1
+        for neighbour in self.neighbours(point):
+
+            if self.array[neighbour[0], neighbour[1]] == 1\
+                    and self.visited[neighbour[0],
+                                     neighbour[1]] == 0:
+                self.pixels.append(list(neighbour))
+                self.dfs(neighbour)
+
+    def dfs_all(self):
+
+        """start dfs on all black pixels"""
+
+        for point_x in range(self.rows):
+            for point_y in range(self.cols):
+
+                # start dfs only on black pixel
+
+                if self.visited[point_x, point_y] == 0 and \
+                                self.array[point_x, point_y] == 1:
+
+                    self.pixels = [[point_x, point_y]]
+                    self.dfs((point_x, point_y))
+                    new_sign = NewSign(np.array(self.pixels))
+                    new_sign.save()
---
+++ save_images.py
@@ -0,0 +1,45 @@
+import numpy as np
+from scipy import misc
+
+
+class NewSign:
+    def __init__(self, pixels):
+        self.pixels = pixels
+        self.north, self.south, self.west, self.east = \
+            self.find_borders()
+        self.width = self.east - self.west
+        self.height = self.south - self.north
+        self.normalized = self.normalize_image()
+        self.bitmap = self.make_bitmap()
+
+    def find_borders(self):
+
+        """find extremal values of
+        coefficients"""
+
+        return np.min(self.pixels, axis=0)[0], \
+               np.max(self.pixels, axis=0)[0], \
+               np.min(self.pixels, axis=0)[1], \
+               np.max(self.pixels, axis=0)[1]
+
+    def normalize_image(self):
+
+        """normalize image as if it starts from [0, 0]"""
+
+        return self.pixels - np.full((self.pixels.shape[0], 2),
+                                    [self.north, self.west])
+
+    def make_bitmap(self):
+
+        """save a bitmap of separated sign"""
+
+        bitmap = np.full((16, 16, 3),
+                         [255, 255, 255])
+        for pixel in self.normalized:
+            if pixel[0] < 16 and pixel[1] < 16:
+                bitmap[pixel[0], pixel[1]] = [0, 0, 0]
+
+        return bitmap
+
+    def save(self):
+        misc.imsave(self.bitmap)
